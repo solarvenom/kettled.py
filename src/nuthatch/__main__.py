8cedd7d05d756e4778d14152929c2632c2c88ece
solarvenom <gremlinmancer@gmail.com>
2024-10-16 19:13:38 +0200

    add explicit function imports from built-in packages in main

--- src/nuthatch/__main__.py
+++ src/nuthatch/__main__.py
@@ -1,30 +1,31 @@
 
-import sys, time
+from sys import exit, argv
+from time import sleep
 from nuthatch.daemon import Daemon
  
 class NuthatchDaemon(Daemon):
     def run(self):
         while True:
-            time.sleep(1)
+            sleep(1)
  
 def main() -> None:
     daemon = NuthatchDaemon()
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
+    if len(argv) == 2:
+        if 'start' == argv[1]:
             daemon.start()
-        elif 'stop' == sys.argv[1]:
+        elif 'stop' == argv[1]:
             daemon.stop()
-        elif 'restart' == sys.argv[1]:
+        elif 'restart' == argv[1]:
             daemon.restart()
-        elif 'status' == sys.argv[1]:
+        elif 'status' == argv[1]:
             daemon.status()
         else:
             print("Unknown command")
-            sys.exit(2)
-        sys.exit(0)
+            exit(2)
+        exit(0)
     else:
-        print("usage: %s start|stop|restart" % sys.argv[0])
-        sys.exit(2)
+        print("usage: %s start|stop|restart" % argv[0])
+        exit(2)
 
 if __name__ == "__main__":
     main()
