e9a86d931f0c27783ab285fc033323ff7e20c105
solarvenom <gremlinmancer@gmail.com>
2024-10-19 20:20:12 +0200

    add list test

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,12 +1,13 @@
+from datetime import datetime
 from cauldrond.scheduler import Scheduler
 import seeds
 
-def test_scheduler_init():
+def test_init():
     test_instance = Scheduler()
     assert test_instance.storage == {}
     assert test_instance.index == {}
 
-def test_scheduler_set():
+def test_set():
     test_instance = Scheduler()
     test_instance.set(
         name=seeds.EVENT_NAME,
@@ -16,3 +17,15 @@ def test_scheduler_set():
     
     assert test_instance.storage[seeds.EVENT_TIMESTAMP][seeds.EVENT_NAME]() == seeds.EVENT_CALLBACK_VALUE
     assert test_instance.index[seeds.EVENT_NAME] == seeds.EVENT_TIMESTAMP
+
+def test_list(capsys):
+    test_instance = Scheduler()
+    test_instance.set(
+        name=seeds.EVENT_NAME,
+        date_time=seeds.EVENT_DATE,
+        callback=seeds.EVENT_CALLBACK()
+    )
+    ISOFORMAT = datetime.fromtimestamp(seeds.EVENT_TIMESTAMP).isoformat()
+    test_instance.list()
+    captured = capsys.readouterr()
+    assert captured.out == f"| 1 | {seeds.EVENT_NAME} | {ISOFORMAT} |\n"
