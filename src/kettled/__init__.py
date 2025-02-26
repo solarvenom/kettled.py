26c82167fb0fa255ef67e724ccb59badbda10b13
solarvenom <contact@solarvenom.com>
2025-02-26 12:26:24 +0100

    add event parameters to exported enums

--- src/kettled/__init__.py
+++ src/kettled/__init__.py
@@ -2,13 +2,15 @@ from kettled.handlers.import_handler import ImportHandler as Kettled
 from kettled.constants.enums.recurrency_options_enum import RECURRENCY_OPTIONS_ENUM
 from kettled.constants.enums.relative_datetime_options_enum import RELATIVE_DATETIME_OPTIONS_ENUM
 from kettled.constants.enums.fallback_options_enum import FALLBACK_DIRECTIVES_ENUM
+from kettled.constants.enums.event_parameters_enum import EVENT_PARAMETERS_ENUM
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "solarvenom"
 
 __all__ = [
     "Kettled", 
     "RECURRENCY_OPTIONS_ENUM",
     "RELATIVE_DATETIME_OPTIONS_ENUM",
-    "FALLBACK_DIRECTIVES_ENUM"
+    "FALLBACK_DIRECTIVES_ENUM",
+    "EVENT_PARAMETERS_ENUM"
 ]
